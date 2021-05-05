402d3f1c171dd21680bd1fdb7bbb6b51e47f5790
danielrossyamitrano <danielrossyamitrano@gmail.com>
2021-05-04 21:05:03 -0300

    Version 1.5.0 + Ahora el archivo de guardado lleva el formato actual de los árboles   de diálogo, aunque la única key que puede guardar del head es la   de los locutores. * Actualizado el número de versión en README.md

--- backend/dialog.py
+++ backend/dialog.py
@@ -5,23 +5,43 @@ from .system import System
 
 class Dialog:
     name = ''
-    nodes = None
+    tree = None
 
     @classmethod
     def init(cls):
-        cls.nodes = {}
+        cls.tree = {
+            'head': {
+                'class': "scripted",
+                "conditional_tags": {},
+                "style": "",
+                "locutors": [],
+                "about": "",
+                "icon": None,
+                "keywords": [],
+                "events": {},
+                "panels": {
+                    "objects": {},
+                    "themes": {}
+                }
+            },
+            'body': {}
+        }
         EventHandler.register(cls.save, 'CreateDialog')
 
     @classmethod
     def save(cls, event):
+        nodes = cls.tree['body']
         for node in event.data['nodes']:
-            cls.nodes[str(node)] = {'type': node.tipo}
+            text = System.data[int(node)]
+            nodes[str(node)] = {'type': node.tipo}
             if node.tipo != 'leaf':
-                cls.nodes[str(node)]['leads'] = node.lead
-            cls.nodes[str(node)]['txt'] = System.data[int(node)]
-            cls.nodes[str(node)]['from'] = node.locutor_name
-            cls.nodes[str(node)]['to'] = node.interlocutor.locutor_name if node.interlocutor is not None else ''
-        guardar_json('data/output.json', cls.nodes)
+                nodes[str(node)]['leads'] = node.lead
+            nodes[str(node)]['txt'] = text
+            nodes[str(node)]['from'] = node.locutor_name
+            if node.locutor_name not in cls.tree['head']['locutors']:
+                cls.tree['head']['locutors'].append(node.locutor_name)
+            nodes[str(node)]['to'] = node.interlocutor.locutor_name if node.interlocutor is not None else ''
+        guardar_json('data/output.json', cls.tree)
 
 
 Dialog.init()
